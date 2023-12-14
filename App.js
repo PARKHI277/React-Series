0a8b0ef0859181254674db1ec47b18b9d248efb4
Parkhi <parkhigarg277@gmail.com>
2023-12-14 13:18:56 +0530

    building app

--- App.js
+++ App.js
@@ -1,4 +1,249 @@
- /*
+/*
+  Header 
+    - logo
+    - nav items
+  body
+    - search
+    - contaniner
+    - restro card
+  footer
+    - copyright
+    - links
+    - address
+    - contact
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//functional Component
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+// prop is an object
+
+const RestroCard = (props) => {
+  const { resData } = props;
+
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.treebo.com/blog/wp-content/uploads/2018/08/Raj-Kachori.jpg"
+      />
+
+      <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisnes.join(", ")}</h4>
+      <h4>{resData.data.avgRating}</h4>
+      <h4>{resData.data.costForTwo}</h4>
+      <h4>{resData.data.deliveryTime}</h4>
+    </div>
+  );
+};
+
+const reslistObj = [
+  {
+    type: "restaurtant",
+    data: {
+      id: "46568956788",
+
+      type: "F",
+      name: "KFC",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "465689b56788",
+
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      type: "F",
+      id: "46898",
+
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "4612898",
+
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "45688",
+
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "456898",
+
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "456898",
+
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "4656898",
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "4657898",
+      type: "F",
+
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "46576898",
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+  {
+    type: "restaurtant",
+    data: {
+      id: "465768798",
+      type: "F",
+      name: "KFCY",
+      costForTwo: 40000,
+      cuisnes: ["Burgers", "Biryani", "American", "Snacks", "Fast Foods"],
+      avgRating: "3.8",
+      totalRatings: "500",
+      deliveryTime: "38 minutes",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {reslistObj.map((restaurtant) => (
+          <RestroCard key={restaurtant.data.id} resData={restaurtant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
+
+/*
  1. Hot module , building , mininfy , clear your code
  2. super fast build algorithim
  3. image optimiazation
@@ -10,15 +255,14 @@
  9. Transistive Dependencies
 */
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 /* -> jsx - this is not html inside javascript. It is like html syntax
    -> Bable convert jsx to react code
 */
- const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
 
- const elem = <span>React Element</span>
+/*
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+
+const elem = <span>React Element</span>;
 const title = (
   <h1 className="head" tableIndex="5">
     {elem}
@@ -40,29 +284,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 /*
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
